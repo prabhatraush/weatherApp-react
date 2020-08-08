ee82f84f2444c2c7e9b5f3ba8ae72d1f2399b562
Prabhat Raushan <prabhatraush@gmail.com>
2020-08-09 02:30:05 +0530

    mixed block content fixed

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
-export const base_url = "http://api.openweathermap.org/data/2.5/";
+export const base_url = "https://api.openweathermap.org/data/2.5/";
 export const key = "54391dbba72ecf2c12392e443a719024";
